1738dd37049e3f3da3ea14eac002bfd2a54d5b39
ivoryeee <psa109@sookmyung.ac.kr>
2025-01-02 17:17:49 +0900

    [test] ci 테스트

--- src/main/java/org/example/DemoController.java
+++ src/main/java/org/example/DemoController.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
